0cabe249a747ed4bae2d688cf3343ab29c696c05
Bodrug <vladimir.bodrug@p4b.ro>
2023-09-05 18:13:10 +0300

    create the arraysIntersection2 and PlusOne tests

--- src/main/java/org/rentalsavg/ArraysHints.java
+++ src/main/java/org/rentalsavg/ArraysHints.java
@@ -1,6 +1,10 @@
 package org.rentalsavg;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ArraysHints {
 	public void rotateArray(int[] arr, int shift) {
@@ -19,6 +23,95 @@ public class ArraysHints {
 			}
 		}
 		return Arrays.copyOfRange(arr, 0, index);
+	}
+
+	public int getSingleUniqueElementFromDoublesArray(int[] arr) {
+		int result = 0;
+		for (int element : arr) {
+			result ^= element;
+		}
+		return result;
+	}
+
+	public int[] arraysIntersection1(int[] arr1, int[] arr2) {
+		Map<Integer, Integer> occurences;
+		int[] result;
+		if (arr1.length < arr2.length) {
+			occurences = getMapOccurences(arr1);
+			result = getIntersectionFromMap(arr2, occurences);
+		} else {
+			occurences = getMapOccurences(arr2);
+			result = getIntersectionFromMap(arr1, occurences);
+		}
+
+		return result;
+	}
+
+	public int[] arraysIntersection2(int[] arr1, int[] arr2) {
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		int indexResult = 0, indexArr1 = 0, indexArr2 = 0;
+
+		while ((indexArr1 < arr1.length) && (indexArr2 < arr2.length)) {
+			if (arr1[indexArr1] == arr2[indexArr2]) {
+				arr1[indexResult++] = arr1[indexArr1];
+				indexArr1++;
+				indexArr2++;
+			} else if (arr1[indexArr1] < arr2[indexArr2]) {
+				++indexArr1;
+			} else {
+				++indexArr2;
+			}
+		}
+
+		return Arrays.copyOfRange(arr1, 0, indexResult);
+	}
+
+	public int[] plusOne(int[] numbers) {
+		for (int i = numbers.length-1; i >= 0; --i) {
+			if (numbers[i] < 9) {
+				numbers[i]++;
+				return numbers;
+			} else {
+				numbers[i] = 0;
+			}
+		}
+
+		int[] result = new int[numbers.length +1];
+		result[0] = 1;
+
+		return result;
+	}
+
+	private int[] getIntersectionFromMap(int[] arr, Map<Integer, Integer> occurences) {
+		List<Integer> intersections = new ArrayList<>();
+		for (int elem : arr) {
+			if (occurences.containsKey(elem)) {
+				intersections.add(elem);
+				int currentVal = occurences.get(elem);
+				if (currentVal == 1) {
+					occurences.remove(elem);
+				} else {
+					occurences.put(elem, currentVal-1);
+				}
+			}
+		}
+		int[] result = new int[intersections.size()];
+
+		for(int i = 0; i < intersections.size(); ++i) {
+			result[i] = intersections.get(i);
+		}
+
+		return result;
+	}
+
+	private Map<Integer, Integer> getMapOccurences(int[] arr) {
+		Map<Integer, Integer> occurences = new HashMap<>();
+		for (int elem : arr) {
+			occurences.put(elem, occurences.getOrDefault(elem, 0)+1);
+		}
+
+		return occurences;
 	}
 
 	private void reverseArray(int[] arr, int start, int end) {
--- src/test/java/org/rentalsavg/ArraysHintsTest.java
+++ src/test/java/org/rentalsavg/ArraysHintsTest.java
@@ -40,4 +40,40 @@ class ArraysHintsTest {
 		Integer[] uniqueResult = arraysHints.getUniqueElements(arr);
 		assertArrayEquals(new Integer[]{1,2,3,4,5,6}, uniqueResult);
 	}
+
+	@Test
+	void getUniqueElement() {
+		int[] arr = new int[]{2, 5, 5, 6, 7, 8, 7, 6, 8};
+		int unique = arraysHints.getSingleUniqueElementFromDoublesArray(arr);
+		assertEquals(2, unique);
+	}
+
+	@Test
+	void checkArraysIntersections1() {
+		int[] arr1 = new int[]{2, 22, 1, 2, 2, 5, 6, 8};
+		int[] arr2 = new int[]{34, 2, 7, 2, 15, 8};
+		int[] uniqueBiggerFirst = arraysHints.arraysIntersection1(arr1, arr2);
+
+		int[] arr3 = new int[]{4, 1, 2, 5, 8, 2};
+		int[] arr4 = new int[]{2, 8, 2};
+		int[] uniqueBiggerSecond = arraysHints.arraysIntersection1(arr3, arr4);
+
+		assertAll(
+				() -> assertArrayEquals(new int[]{2, 2, 8}, uniqueBiggerFirst),
+				() -> assertArrayEquals(new int[]{2, 8, 2}, uniqueBiggerSecond)
+		);
+	}
+
+	@Test
+	@DisplayName("It should return the same array with the number, compound from all the digits, incremented with 1")
+	void checkPlusOne() {
+		int[] numbers = new int[]{1,2,3,4,5,6};
+		int[] numbers2 = new int[]{9};
+		int[] numbers3 = new int[]{8,9,9};
+		assertAll(
+				() -> assertArrayEquals(new int[]{1,2,3,4,5,7}, arraysHints.plusOne(numbers)),
+				() -> assertArrayEquals(new int[]{1, 0}, arraysHints.plusOne(numbers2)),
+				() -> assertArrayEquals(new int[]{9,0,0}, arraysHints.plusOne(numbers3))
+		);
+	}
 }
